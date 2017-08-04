96ee11e81ebd3295d3b0067f86299ae8e1d5cc94
vitoor-s <vitorsugaya5@gmail.com>
2017-08-04 12:45:34 -0300

    Adicionada activity maps, dois marcadores, e permissao de acesso a localizacao

--- app/src/main/java/br/com/secompufscar/secomp_ufscar/MapsActivity.java
+++ app/src/main/java/br/com/secompufscar/secomp_ufscar/MapsActivity.java
@@ -38,9 +38,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //botoes de zoom
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        LatLng DC = new LatLng(-21.979509, -47.880528);
+        LatLng BentoPrado = new LatLng(-21.983667, -47.881668);
+        mMap.addMarker(new MarkerOptions().position(DC).title("Departamento de Computação"));
+        mMap.addMarker(new MarkerOptions().position(BentoPrado).title("Anfiteatro Bento Prado Júnior"));
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(DC));
     }
 }
