c0b7f56f358380145b16049e8cffce9adc97a2a7
vitoor-s <vitorsugaya5@gmail.com>
2017-08-29 19:48:40 -0300

    "Adicionado AlertDialog dentro do método onCreate e criação de opção para não obter localização usando dados"

--- app/src/main/java/br/com/secompufscar/secomp_ufscar/MapsActivity.java
+++ app/src/main/java/br/com/secompufscar/secomp_ufscar/MapsActivity.java
@@ -1,13 +1,17 @@
 package br.com.secompufscar.secomp_ufscar;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -18,14 +22,16 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener {
 
     //TODO: Pergunta 3g ou dados? usar opçao com rede ou apenas gps
 
     private static final String TAG = "MapsActivity";
     private GoogleMap mMap;
     private LocationManager locationManager;
+    private AlertDialog.Builder alertBuilder;
     private int Local = 0;
+    private int Nav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,12 +43,32 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
 
         Intent intent = getIntent();
-        if(intent != null){
+        if (intent != null) {
             Bundle params = intent.getExtras();
-            if(params != null){
+            if (params != null) {
                 Local = params.getInt("Local");
             }
         }
+
+        alertBuilder = new AlertDialog.Builder(this);
+
+        //NAO SEI SE ESTA CORRETO /////////////////////////
+        alertBuilder.setMessage("Deseja usar dados móveis para obter sua localização?");
+
+        alertBuilder.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                Nav = 1;
+            }
+        });
+
+        alertBuilder.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                Nav = 0;
+            }
+        });
+        AlertDialog dialog = alertBuilder.create();
+        dialog.show();
+
     }
 
     @Override
@@ -51,20 +77,38 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         mMap = googleMap;
         mMap.getUiSettings().setZoomControlsEnabled(true);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-       try {
-            locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-            mMap = googleMap;
+        if (Nav == 1) {
+            try {
+                //locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+                //mMap = googleMap;
 
-            Criteria criteria = new Criteria();
+                Criteria criteria = new Criteria();
 
-            locationManager.getBestProvider(criteria, true);
+                //locationManager.getBestProvider(criteria, true);
+                locationManager.getAllProviders();
+                locationManager.requestLocationUpdates(locationManager.getBestProvider(criteria, true),3000, 10, this);
 
-            //botoes de zoom e sua localizacao
-            mMap.getUiSettings().setZoomControlsEnabled(true);
-            mMap.setMyLocationEnabled(true);
-        } catch (SecurityException ex) {
-            Log.e(TAG, "Error while acquiring location", ex);
+                //botoes de zoom e sua localizacao
+                //mMap.getUiSettings().setZoomControlsEnabled(true);
+                mMap.setMyLocationEnabled(true);
+            } catch (SecurityException ex) {
+                Log.e(TAG, "Error while acquiring location", ex);
+            }
+        } else {
+            locationManager.getProvider(LocationManager.GPS_PROVIDER);
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3000, 10, this);
         }
 
         LatLng DC = new LatLng(-21.979509, -47.880528);
@@ -91,6 +135,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 mMap.moveCamera(CameraUpdateFactory.zoomTo(17));
                 break;
         }
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
 
     }
 }
